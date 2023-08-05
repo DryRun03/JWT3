5700caa97433fc5c75ae9cfa3ff50719e191e41c
Pankaj Garg <pankaj.garg@oodles.io>
2023-08-05 16:26:30 +0530

    IntelliJ File

---
+++ src/main/java/io/pankaj/Jwt3Application.java
@@ -0,0 +1,13 @@
+package io.pankaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jwt3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/pankaj/Jwt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pankaj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
