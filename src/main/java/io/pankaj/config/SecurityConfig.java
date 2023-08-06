0f87b2f1cd9a339ea60a89440baba189c4983761
Pankaj Garg <pankaj.garg@oodles.io>
2023-08-06 14:49:00 +0530

    Added JWT dependencies 0.11.5

--- src/main/java/io/pankaj/config/SecurityConfig.java
+++ src/main/java/io/pankaj/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package io.pankaj.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,4 +27,9 @@ public class SecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
 }
