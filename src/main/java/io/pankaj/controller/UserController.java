88aef1e9739f95754e393b8dd409d4b77e815f4a
Pankaj Garg <pankaj.garg@oodles.io>
2023-08-06 14:10:47 +0530

    Added getCurrentUser API

--- src/main/java/io/pankaj/controller/UserController.java
+++ src/main/java/io/pankaj/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RequestMapping("/home")
@@ -21,5 +22,10 @@ public class UserController {
     @GetMapping("/user")
     public List<User> getAllUsers(){
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/currentUser")
+    public String getLoggedInUser(Principal principal){
+        return principal.getName();
     }
 }
